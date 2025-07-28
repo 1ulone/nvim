96d3fac111ffc0a24bbf2d52177ac3f960062dc8
W <ulone@ulone-master.bbrouter>
2025-07-29 05:03:13 +0700

    initialize
    Init config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+
+        },
+        keys = {
+            { "<leader>lg", function() require("snacks").lazygit() end, desc = "Open Lazygit" },
+            { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Open Lazygit Logs" },
+            { "<leader>rN", function() require("snacks").rename.rename_file() end, desc = "Rename Current file" },
+            { "<leader>dB", function() require("snacks").bufDelete() end, desc = "Delete or Close buffer?" },
+
+            { "<leader>ff", function() require("snacks").picker.files() end, desc = "find files" },
+            { "<leader>pc", function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Neovim Config file" },
+            { "<leader>ps", function() require("snacks").picker.grep() end, desc = "Grep word" },
+            { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "Search visual selection or word", mode = {"n", "x"} },
+            { "<leader>gs", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc="pick and switch Git Branches" },
+        }
+    },
+    {
+        "folke/todo-comments.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        keys = {
+            { "<leader>pt", function() require("snacks").picker.todo_comments() end, desc = "Search Todo's" },
+            { "<leader>pT", function() require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc="Search Todo/Fix/Fixme" }
+        }
+    }
+}
